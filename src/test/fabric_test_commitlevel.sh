4046bb90a2e382ecb906890376a709995846e52b
rameshthoomu <rameshbabu.thoomu@gmail.com>
2017-02-18 20:32:17 +0000

    fabric and fabric-ca commit levels
    Change-Id: I082fb58418134de5830c3be624dc22182a6e3f7f
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

---
+++ src/test/fabric_test_commitlevel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Please change below commits if you feel end-to-end works
+
+export FABRIC_COMMIT=5d9e4ede298ab646ac918dc5b034c7d319dd1d9a
+export FABRIC_CA_COMMIT=bf8fb4d5e497217cd6125025830aa6870de442aa
