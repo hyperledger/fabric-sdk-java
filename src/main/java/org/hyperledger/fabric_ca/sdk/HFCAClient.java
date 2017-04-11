7e72881cfe1e160b1d36987ae5b64c1befb1fee4
Toshio Suganuma <suganuma@jp.ibm.com>
2017-04-12 02:36:45 +0900

    FAB-3054 Remove /api/v1/cfssl prefix to fabric-ca
    Confirmed all work ok for IntegrationSuite
    
    Change-Id: I5243bd3b58d23d0f11ad74155ebecb4408537287
    Signed-off-by: Toshio Suganuma <suganuma@jp.ibm.com>

--- src/main/java/org/hyperledger/fabric_ca/sdk/HFCAClient.java
+++ src/main/java/org/hyperledger/fabric_ca/sdk/HFCAClient.java
@@ -101,7 +101,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 public class HFCAClient implements MemberServices {
     private static final Log logger = LogFactory.getLog(HFCAClient.class);
-    private static final String HFCA_CONTEXT_ROOT = "/api/v1/cfssl/";
+    private static final String HFCA_CONTEXT_ROOT = "/";
     private static final String HFCA_ENROLL = HFCA_CONTEXT_ROOT + "enroll";
     private static final String HFCA_REGISTER = HFCA_CONTEXT_ROOT + "register";
     private static final String HFCA_REENROLL = HFCA_CONTEXT_ROOT + "reenroll";
