584f4561fed45d484b12e944c5a8570708792ccd
Chris Murphy <chrism@fast.au.fujitsu.com>
2017-06-09 20:33:11 +1000

    FAB-4454 Increase junit coverage for Config.java
    . Modified fabric/sdk/helper/ConfigTest.java to improve coverage
    . Added fabric_ca/sdj/helper/ConfigTest.java
    . Enhanced TestConfigHelper.java to properly clear the current config
    . Added a new helper method in TestConfigHelper.java for fabric_ca
    
    Change-Id: Ia88d6471eb314f04cea2f14e77ea52d974ce6faf
    Signed-off-by: Chris Murphy <chrism@fast.au.fujitsu.com>

--- src/test/java/org/hyperledger/fabric/sdk/TestConfigHelper.java
+++ src/test/java/org/hyperledger/fabric/sdk/TestConfigHelper.java
@@ -4,7 +4,7 @@
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,6 +12,8 @@
  *  limitations under the License.
  */
 package org.hyperledger.fabric.sdk;
+
+import java.util.Properties;
 
 import org.hyperledger.fabric.sdk.helper.Config;
 
@@ -27,30 +29,72 @@ public class TestConfigHelper {
      * clearConfig "resets" Config so that the Config testcases can run without interference from other test suites.
      * Depending on what order JUnit decides to run the tests, Config could have been instantiated earlier and could
      * contain values that make the tests here fail.
+     *
      * @throws SecurityException
      * @throws NoSuchFieldException
      * @throws IllegalAccessException
      * @throws IllegalArgumentException
      *
      */
-    public void clearConfig() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    public void clearConfig()
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+
         Config config = Config.getConfig();
+
+        // Set the private static variable Config.config = null
         java.lang.reflect.Field configInstance = config.getClass().getDeclaredField("config");
         configInstance.setAccessible(true);
         configInstance.set(null, null);
+
+        // Clear the sdkProperties map - Config.sdkProperties.clear()
+        java.lang.reflect.Field sdkPropInstance = config.getClass().getDeclaredField("sdkProperties");
+        sdkPropInstance.setAccessible(true);
+        Properties sdkProperties = (Properties) sdkPropInstance.get(config);
+        sdkProperties.clear();
+
     }
 
     /**
-     * customizeConfig() sets up the properties listed by env var CONFIG_OVERRIDES
-     * The value of the env var is <i>property1=value1,property2=value2</i> and so on where each <i>property</i> is a property
-     * from the SDK's config file.
+     * clearCaConfig "resets" Config used by fabric_ca so that the Config testcases can run without interference from
+     * other test suites.
+     *
+     * @throws SecurityException
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     *
+     * @see #clearConfig()
+     */
+    public void clearCaConfig()
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+
+        org.hyperledger.fabric_ca.sdk.helper.Config config = org.hyperledger.fabric_ca.sdk.helper.Config.getConfig();
+
+        // Set the private static variable Config.config = null
+        java.lang.reflect.Field configInstance = config.getClass().getDeclaredField("config");
+        configInstance.setAccessible(true);
+        configInstance.set(null, null);
+
+        // Clear the sdkProperties map - Config.sdkProperties.clear()
+        java.lang.reflect.Field sdkPropInstance = config.getClass().getDeclaredField("sdkProperties");
+        sdkPropInstance.setAccessible(true);
+        Properties sdkProperties = (Properties) sdkPropInstance.get(config);
+        sdkProperties.clear();
+
+    }
+
+    /**
+     * customizeConfig() sets up the properties listed by env var CONFIG_OVERRIDES The value of the env var is
+     * <i>property1=value1,property2=value2</i> and so on where each <i>property</i> is a property from the SDK's config
+     * file.
      *
      * @throws NoSuchFieldException
      * @throws SecurityException
      * @throws IllegalArgumentException
      * @throws IllegalAccessException
      */
-    public void customizeConfig() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+    public void customizeConfig()
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         String fabricSdkConfig = System.getenv(CONFIG_OVERRIDES);
         if (fabricSdkConfig != null && fabricSdkConfig.length() > 0) {
             String[] configs = fabricSdkConfig.split(",");
