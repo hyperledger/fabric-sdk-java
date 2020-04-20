6db3212a3c6260dbd95e958acbe03bcdb60873f7
Brett Logan <brett.t.logan@ibm.com>
2020-04-20 09:51:22 -0400

    Simplify Integration Test Suite (#48)
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

---
+++ scripts/pull-fabric-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+set -euo pipefail
+
+# FABRIC_VERSION is set in ci/azure-pipelines.yml
+VERSION=${FABRIC_VERSION:-2.1}
+STABLE_TAG=amd64-${VERSION}-stable
+
+for image in peer orderer tools ccenv javaenv nodeenv; do
+	docker pull -q hyperledger-fabric.jfrog.io/fabric-${image}:${STABLE_TAG}
+	docker tag hyperledger-fabric.jfrog.io/fabric-${image}:${STABLE_TAG} hyperledger/fabric-${image}
+	docker rmi -f hyperledger-fabric.jfrog.io/fabric-${image}:${STABLE_TAG} >/dev/null
+done
+
+docker pull -q hyperledger/fabric-ca:1.4
+docker tag hyperledger/fabric-ca:1.4 hyperledger/fabric-ca
+docker rmi -f hyperledger/fabric-ca:1.4 >/dev/null
