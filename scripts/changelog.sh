a610d6007748fe8c6fdcf39bd1189d36f39608ca
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2020-07-03 14:13:43 +0100

    v2.1.4 release (#64)
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -5,6 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+if [ $# -ne 2 ]; then
+    echo 'Missing required arguments: lastReleaseCommitHash releaseVersion' >&2
+    exit 1
+fi
+
 echo "## $2\n$(date)" >> CHANGELOG.new
 echo "" >> CHANGELOG.new
 git log $1..HEAD  --oneline | grep -v Merge | sed -e "s/\[\(FAB-[0-9]*\)\]/\[\1\](https:\/\/jira.hyperledger.org\/browse\/\1\)/" -e "s/\([0-9|a-z]*\)/* \[\1\](https:\/\/github.com\/hyperledger\/fabric-sdk-java\/commit\/\1)/" >> CHANGELOG.new
