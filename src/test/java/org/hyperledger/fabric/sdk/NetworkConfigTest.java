b12141c25ad928e846fb2a5b587d9439a1d46eee
Mark S. Lewis <mark_lewis@uk.ibm.com>
2020-10-08 12:06:51 +0100

    FABJ-540: Use newline separators when concatenating certificate PEM
    Passing multiple concatenated X.509 certificate PEM to java.security.cert.CertificateFactory.generateCertificates() causes a parsing error. To avoid this, use a newline separator when concatenating certificate PEM bytes to avoid failures in cases where the PEM is not newline terminated.
    
    Also fix the Java chaincode used by integration tests, which was broken by its use of a transient dependency of fabric-chaincode-shim without explicitly declaring it as a dependency.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- src/test/java/org/hyperledger/fabric/sdk/NetworkConfigTest.java
+++ src/test/java/org/hyperledger/fabric/sdk/NetworkConfigTest.java
@@ -18,9 +18,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.EnumSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
@@ -505,9 +505,13 @@ public class NetworkConfigTest {
         Object pemBytes = caInfo.getProperties().get("pemBytes");
 
         assertTrue("Expected byte[], got " + pemBytes.getClass().getTypeName(), pemBytes instanceof byte[]);
+
         String pem = new String((byte[]) pemBytes);
         assertTrue("Missing certificate 1: " + pem, pem.contains("<1>"));
         assertTrue("Missing certificate 2:" + pem, pem.contains("<2>"));
+
+        String[] pemLines = pem.split("\n");
+        assertTrue("Expected at least 2 lines, got:\n" + Arrays.toString(pemLines), pemLines.length >= 2);
     }
 
     // TODO: ca-org1 not defined
