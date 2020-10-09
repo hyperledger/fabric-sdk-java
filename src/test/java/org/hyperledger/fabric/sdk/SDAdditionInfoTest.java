4f772510c66eb044d4990509b7e90f4795fa6bab
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2020-10-09 09:31:24 +0100

    FABJ-540: Use newline separators when concatenating certificate PEM
    Passing multiple concatenated X.509 certificate PEM to java.security.cert.CertificateFactory.generateCertificates() causes a parsing error. To avoid this, use a newline separator when concatenating certificate PEM bytes to avoid failures in cases where the PEM is not newline terminated.
    
    Also fix the Java chaincode used by integration tests, which was broken by its use of a transient dependency of fabric-chaincode-shim without explicitly declaring it as a dependency.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

---
+++ src/test/java/org/hyperledger/fabric/sdk/SDAdditionInfoTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2020 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.fabric.sdk;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+
+import org.hyperledger.fabric.sdk.exception.ServiceDiscoveryException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SDAdditionInfoTest {
+    private static byte[][] asByteArrays(String... strings) {
+        return Arrays.stream(strings)
+                .map(cert -> cert.getBytes(StandardCharsets.UTF_8))
+                .toArray(size -> new byte[size][]);
+    }
+
+    private static Channel.SDOrdererAdditionInfo newOrdererAdditionInfo(byte[][] tlsCerts, byte[][] tlsIntermediateCerts) {
+        return new Channel.SDOrdererAdditionInfo() {
+            @Override
+            public String getEndpoint() {
+                return null;
+            }
+
+            @Override
+            public Properties getProperties() {
+                return null;
+            }
+
+            @Override
+            public String getMspId() {
+                return null;
+            }
+
+            @Override
+            public Channel getChannel() {
+                return null;
+            }
+
+            @Override
+            public HFClient getClient() {
+                return null;
+            }
+
+            @Override
+            public byte[][] getTLSCerts() {
+                return tlsCerts;
+            }
+
+            @Override
+            public byte[][] getTLSIntermediateCerts() {
+                return tlsIntermediateCerts;
+            }
+
+            @Override
+            public Map<String, Orderer> getEndpointMap() {
+                return null;
+            }
+
+            @Override
+            public boolean isTLS() {
+                return false;
+            }
+        };
+    }
+
+    private static Channel.SDPeerAdditionInfo newPeerAdditionInfo(byte[][] tlsCerts, byte[][] tlsIntermediateCerts) {
+        return new Channel.SDPeerAdditionInfo() {
+            @Override
+            public String getName() {
+                return null;
+            }
+
+            @Override
+            public String getMspId() {
+                return null;
+            }
+
+            @Override
+            public String getEndpoint() {
+                return null;
+            }
+
+            @Override
+            public Channel getChannel() {
+                return null;
+            }
+
+            @Override
+            public HFClient getClient() {
+                return null;
+            }
+
+            @Override
+            public byte[][] getTLSCerts() {
+                return tlsCerts;
+            }
+
+            @Override
+            public byte[][] getTLSIntermediateCerts() {
+                return tlsIntermediateCerts;
+            }
+
+            @Override
+            public Map<String, Peer> getEndpointMap() {
+                return null;
+            }
+
+            @Override
+            public Properties getProperties() {
+                return null;
+            }
+
+            @Override
+            public boolean isTLS() {
+                return false;
+            }
+        };
+    }
+
+    @Test
+    public void testOrdererSeparatesCertificatesWithNewlines() throws ServiceDiscoveryException {
+        byte[][] tlsCerts = asByteArrays("a", "b");
+        byte[][] intermediateCerts = asByteArrays("c", "d");
+        Channel.SDOrdererAdditionInfo additionInfo = newOrdererAdditionInfo(tlsCerts, intermediateCerts);
+
+        byte[] allCerts = additionInfo.getAllTLSCerts();
+
+        String[] allCertLines = new String(allCerts, StandardCharsets.UTF_8).split("\n");
+        assertTrue("Expected at least 4 lines, got:\n" + Arrays.toString(allCertLines), allCertLines.length >= 4);
+    }
+
+    @Test
+    public void testPeerSeparatesCertificatesWithNewlines() throws ServiceDiscoveryException {
+        byte[][] tlsCerts = asByteArrays("a", "b");
+        byte[][] intermediateCerts = asByteArrays("c", "d");
+        Channel.SDPeerAdditionInfo additionInfo = newPeerAdditionInfo(tlsCerts, intermediateCerts);
+
+        byte[] allCerts = additionInfo.getAllTLSCerts();
+
+        String[] allCertLines = new String(allCerts, StandardCharsets.UTF_8).split("\n");
+        assertTrue("Expected at least 4 lines, got:\n" + Arrays.toString(allCertLines), allCertLines.length >= 4);
+    }
+}
