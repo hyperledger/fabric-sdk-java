b1e42fa1bb139ee13bdf83e15f3e5a44adba9f22
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2020-10-09 14:56:06 +0100

    FABJ-540: Use newline separators when concatenating certificate PEM (#88)
    Passing multiple concatenated X.509 certificate PEM to java.security.cert.CertificateFactory.generateCertificates() causes a parsing error. To avoid this, use a newline separator when concatenating certificate PEM bytes to avoid failures in cases where the PEM is not newline terminated.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- src/main/java/org/hyperledger/fabric/sdk/NetworkConfig.java
+++ src/main/java/org/hyperledger/fabric/sdk/NetworkConfig.java
@@ -798,7 +798,7 @@ public class NetworkConfig {
             }
 
             byte[] pemBytes = getJsonValueAsList(jsonTlsCaCerts.get("pem"), NetworkConfig::getJsonValueAsString).stream()
-                    .collect(Collectors.joining())
+                    .collect(Collectors.joining("\n"))
                     .getBytes();
             props.put("pemBytes", pemBytes);
         }
