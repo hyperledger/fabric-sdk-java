b66bc3ba806a1afadce3b66872c626c1e55d35f7
Peng-Sheng,Chen <peng_sheng_chen@hotmail.com>
2021-07-26 17:41:19 +0800

    Update Endpoint.java
    modfiy Endpoint.java that can read config attribute grpcOptions: grpc.NettyChannelBuilderOption.maxInboundMessageSize

--- src/main/java/org/hyperledger/fabric/sdk/Endpoint.java
+++ src/main/java/org/hyperledger/fabric/sdk/Endpoint.java
@@ -387,18 +387,23 @@ class Endpoint {
                 }
             }
 
-            final Method method = channelBuilder.getClass().getMethod(methodName, classParms);
-
-            method.invoke(channelBuilder, parmsArray);
-
-            if (logger.isTraceEnabled()) {
-                logger.trace(format("Endpoint with url: %s set managed channel builder method %s (%s) ", url,
-                        method, Arrays.toString(parmsArray)));
-
+            if ("maxInboundMessageSize".contains(methodName)) {
+                this.channelBuilder.maxInboundMessageSize(Integer.parseInt(props.getProperty("grpc.NettyChannelBuilderOption.maxInboundMessageSize")));
+                logger.trace(
+                    format(
+                        "Set grpc.NettyChannelBuilderOption.maxInboundMessageSizeEndpoint: %s ",
+                        props.getProperty("grpc.NettyChannelBuilderOption.maxInboundMessageSize")));
+            } else {
+              final Method method = channelBuilder.getClass().getMethod(methodName, classParms);
+              method.invoke(channelBuilder, parmsArray);
+              if (logger.isTraceEnabled()) {
+                logger.trace(
+                    format(
+                        "Endpoint with url: %s set managed channel builder method %s (%s) ",
+                        url, method, Arrays.toString(parmsArray)));
+              }
             }
-
         }
-
     }
 
     AbstractMap.SimpleImmutableEntry<PrivateKey, X509Certificate[]> getClientTLSProps(Properties properties) {
