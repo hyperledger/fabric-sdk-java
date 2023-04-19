107fd1fff5f5c94b57b148d677f28db810e1bba7
Mark S. Lewis <Mark_Lewis@uk.ibm.com>
2023-04-19 14:29:19 +0100

    Document deprecation as of Fabric v2.5 (#279)
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

---
+++ src/main/java/org/hyperledger/fabric/sdk/exception/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2023 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @deprecated As of Fabric v2.5, replaced by the <a href="https://hyperledger.github.io/fabric-gateway/">Fabric Gateway client API</a>.
+ */
+package org.hyperledger.fabric.sdk.exception;
